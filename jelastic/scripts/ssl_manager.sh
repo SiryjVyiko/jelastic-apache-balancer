dfd67203ba5fca0f22395ff8151292be8ab08732
SiryjVyiko <dz@hivext.net>
2014-11-24 18:57:26 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -10,7 +10,7 @@ SERVER_XML_CONFIG="/etc/httpd/conf.d/ssl.conf";
 
 function _enableSSL(){
         local default_httpd_conf="/etc/httpd/conf/httpd.conf"
-        grep -q "OPENSSL_NO_DEFAULT_ZLIB" $default_httpd_conf || echo "export OPENSSL_NO_DEFAULT_ZLIB=1" >>  $default_httpd_conf;
+        #grep -q "OPENSSL_NO_DEFAULT_ZLIB" $default_httpd_conf || echo "export OPENSSL_NO_DEFAULT_ZLIB=1" >>  $default_httpd_conf;
         local err;
         doAction keystore DownloadKeys;
         err=$?; [[ ${err} -gt 0 ]] && exit ${err};
