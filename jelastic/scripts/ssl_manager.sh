0f1323b43b42016237ae824d55b88269a29556fe
QA <qa@localhost.localdomain>
2014-11-24 10:19:12 +0200

    changes to setup script

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+#  $Id$
+#  $Revision$
+#  $Date$
+#  $Author$
+#  $HeadURL$
+
+_SED=`which sed`;
+SERVER_XML_CONFIG="/etc/httpd/conf.d/ssl.conf";
 
 function _enableSSL(){
-return 0;
+        local default_httpd_conf="/etc/sysconfig/httpd"
+        grep -q "OPENSSL_NO_DEFAULT_ZLIB" $default_httpd_conf || echo "export OPENSSL_NO_DEFAULT_ZLIB=1" >>  $default_httpd_conf;
+        local err;
+        doAction keystore DownloadKeys;
+        err=$?; [[ ${err} -gt 0 ]] && exit ${err};
+        $_SED -i 's/SSLEngine=\"off\"/SSLEnabled=\"on\"/g' $SERVER_XML_CONFIG;
+        doAction apache2ext enmod ssl > /dev/null 2>&1 ||  { writeJSONResponceErr "result=>4020" "message=>Cannot enable SSL module!"; return 4020; };
 }
 
 function _disableSSL(){
-return 0;
+        local err;
+        doAction keystore remove;
+        err=$?; [[ ${err} -gt 0 ]] && exit ${err};
+        $_SED -i 's/SSLEngine=\"on\"/SSLEnabled=\"off\"/g' $SERVER_XML_CONFIG;
+        doAction apache2ext dismod ssl > /dev/null 2>&1 ||  { writeJSONResponceErr "result=>4021" "message=>Cannot disable SSL module!"; return 4021; };
 }
+
