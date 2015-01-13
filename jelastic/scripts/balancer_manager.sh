281d917b4f4dddb87c44b760b815c2e0e3d72592
Dmitry Zubelevich <dz@jelastic.com>
2015-01-13 12:29:39 +0200

    balancer manager fix

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -10,13 +10,13 @@ function _rebuild_common(){
 
 function _add_common_host(){
     grep -q "${host}" /etc/httpd/conf/virtualhosts.conf && return 0;
-    host_num=`cat /etc/httpd/conf/virtualhosts_http.conf|grep BalancerMember | awk '{print $3}' | sed 's/route=//g | sort | tail -n1';
+    host_num=`cat /etc/httpd/conf/virtualhosts_http.conf|grep BalancerMember | awk '{print $3}' | sed 's/route=//g' | sort | tail -n1;
     let "host_num+=1";
     sed -i '/<Proxy balancer:\/\/myclusterhttp>/a BalancerMember http:\/\/'${host}' route='${host_num}'' /etc/httpd/conf/virtualhosts_http.conf;
     sed -i '/<Proxy balancer:\/\/myclusterajp>/a BalancerMember ajp:\/\/'${host}':8009' /etc/httpd/conf/virtualhosts_ajp.conf;
     sed -i '/<Proxy balancer:\/\/myclusterajp>/a BalancerMember ajp:\/\/'${host}':8009' /etc/httpd/conf/virtualhosts_jk.conf;
     grep -q "${host}" /opt/shared/conf.d/ssl.conf && return 0;
-    sed -i '/<Proxy balancer:\/\/myclusterhttps>/a BalancerMember http:\/\/'${host}' route='${host_num}'' /opt/shared/conf.d/ssl.conf;
+    sed -i '/<Proxy balancer:\/\/myclusterhttps>/a BalancerMember http:\/\/'${host}'' /opt/shared/conf.d/ssl.conf;
 }
 
 
