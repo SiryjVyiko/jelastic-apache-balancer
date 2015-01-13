c4535dde85f073c1098f4cbfe875c9ce4347b0f7
Dmitry Zubelevich <dz@jelastic.com>
2015-01-13 10:45:07 +0200

    ssl sticky ssessions added

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -11,10 +11,10 @@ function _rebuild_common(){
 function _add_common_host(){
     grep -q "${host}" /etc/httpd/conf/virtualhosts.conf && return 0;
     host_num=`cat /etc/httpd/conf/virtualhosts_http.conf|grep BalancerMember | awk '{print $3}' | sed 's/route=//g'`;
-    sed -i '/<Proxy balancer:\/\/myclusterhttp>/a BalancerMember http:\/\/'${host} route=${host_num}'' /etc/httpd/conf/virtualhosts_http.conf;
+    sed -i '/<Proxy balancer:\/\/myclusterhttp>/a BalancerMember http:\/\/'${host}' route=${host_num}' /etc/httpd/conf/virtualhosts_http.conf;
     sed -i '/<Proxy balancer:\/\/myclusterajp>/a BalancerMember ajp:\/\/'${host}':8009' /etc/httpd/conf/virtualhosts_ajp.conf;
     grep -q "${host}" /opt/shared/conf.d/ssl.conf && return 0;
-    sed -i '/<Proxy balancer:\/\/myclusterhttps>/a BalancerMember http:\/\/'${host}'' /opt/shared/conf.d/ssl.conf;
+    sed -i '/<Proxy balancer:\/\/myclusterhttps>/a BalancerMember http:\/\/'${host}' route=${host_num}' /opt/shared/conf.d/ssl.conf;
 }
 
 
