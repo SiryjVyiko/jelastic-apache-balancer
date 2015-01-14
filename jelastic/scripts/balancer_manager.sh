ebf0399170c2512af481706f7d0ae6c6a0c90a37
SiryjVyiko <dz@hivext.net>
2015-01-14 10:16:47 +0200

    Update balancer_manager.sh

--- jelastic/scripts/balancer_manager.sh
+++ jelastic/scripts/balancer_manager.sh
@@ -14,7 +14,6 @@ function _add_common_host(){
     let "host_num+=1";
     sed -i '/<Proxy balancer:\/\/myclusterhttp>/a BalancerMember http:\/\/'${host}' route='${host_num}'' /etc/httpd/conf/virtualhosts_http.conf;
     sed -i '/<Proxy balancer:\/\/myclusterajp>/a BalancerMember ajp:\/\/'${host}':8009' /etc/httpd/conf/virtualhosts_ajp.conf;
-    sed -i '/<Proxy balancer:\/\/myclusterajp>/a BalancerMember ajp:\/\/'${host}':8009' /etc/httpd/conf/virtualhosts_jk.conf;
 }
 
 
