77803662afde46fb3eaeb002273dab0be1c6568d
SiryjVyiko <dz@hivext.net>
2015-02-06 09:48:48 +0200

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -16,6 +16,7 @@ function _enableSSL(){
         err=$?; [[ ${err} -gt 0 ]] && exit ${err};
         $_SED -i 's/SSLEngine=\"off\"/SSLEnabled=\"on\"/g' $SERVER_XML_CONFIG;
         sed -i "/^#LoadModule.*ssl_module.*/ s/^#LoadModule/LoadModule/" /etc/httpd/conf/httpd.conf||  { writeJSONResponceErr "result=>4020" "message=>Cannot enable SSL module!"; return 4020; };
+        ed -i "/^#LoadModule.*ssl_module.*/ s/^#LoadModule/LoadModule/" ${CARTRIDGE_HOME}/versions/2.2/httpd.conf
         service httpd restart >> /dev/null;
 }
 
@@ -25,6 +26,7 @@ function _disableSSL(){
         err=$?; [[ ${err} -gt 0 ]] && exit ${err};
         $_SED -i 's/SSLEngine=\"on\"/SSLEnabled=\"off\"/g' $SERVER_XML_CONFIG;
         sed -i "/^LoadModule.*ssl_module.*/ s/LoadModule/#LoadModule/" /etc/httpd/conf/httpd.conf ||  { writeJSONResponceErr "result=>4021" "message=>Cannot disable SSL module!"; return 4021; };
+        sed -i "/^LoadModule.*ssl_module.*/ s/LoadModule/#LoadModule/" ${CARTRIDGE_HOME}/versions/2.2/httpd.conf
         service httpd restart >> /dev/null;
 }
 
